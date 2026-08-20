8ae5262a5ee048d6ceb72a476e2cb323fbfded57
Finn the Panther <109197508+FinnThePanther@users.noreply.github.com>
2026-08-19 18:02:11 -0700

    test: use supported Semgrep fixture layout

--- .semgrep/tests/tailtag-security.py
+++ .semgrep/tests/tailtag-security.py
@@ -62,6 +62,12 @@ django.utils.safestring.mark_safe(user_input)
 # ok: tailtag.django.mark-safe
 format_html("{}", user_input)
 
+
+def render_with_local_mark_safe(mark_safe: object) -> object:
+    # ok: tailtag.django.mark-safe
+    return mark_safe("not Django")
+
+
 # ruleid: tailtag.django.dynamic-raw-sql
 cursor.execute(f"SELECT * FROM users WHERE name = '{user_input}'")
 # ruleid: tailtag.django.dynamic-raw-sql
