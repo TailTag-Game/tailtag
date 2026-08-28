5ca8f601b38f85dff088475056028d556ac8b334
Finn the Panther <finn@finnthepanther.com>
2026-08-27 19:51:48 -0700

    fix(mobile): remove scaffold signing defaults

--- apps/mobile/test/app/app_test.dart
+++ apps/mobile/test/app/app_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tailtag_mobile/app/router.dart';
 import 'package:tailtag_mobile/main.dart' as app;
 
 void main() {
@@ -11,11 +12,16 @@ void main() {
 
       await tester.pumpAndSettle();
 
-      expect(find.byType(ProviderScope), findsOneWidget);
+      final providerScope = find.byType(ProviderScope);
+      final routerBackedMaterialApp = find.byWidgetPredicate(
+        (widget) => widget is MaterialApp && widget.routerConfig != null,
+      );
+
+      expect(appRouter.routeInformationProvider.value.uri.path, '/');
+      expect(providerScope, findsOneWidget);
+      expect(routerBackedMaterialApp, findsOneWidget);
       expect(
-        find.byWidgetPredicate(
-          (widget) => widget is MaterialApp && widget.routerConfig != null,
-        ),
+        find.descendant(of: providerScope, matching: routerBackedMaterialApp),
         findsOneWidget,
       );
       expect(find.text('TailTag'), findsOneWidget);
